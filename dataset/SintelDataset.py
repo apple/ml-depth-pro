79fcea0908141648787b725fe688a427d2f71228
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-11 17:51:40 +0800

    123

--- dataset/SintelDataset.py
+++ dataset/SintelDataset.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 import numpy as np
 import torch
@@ -11,7 +10,7 @@ from dataset.BaseDataset import BaseDataset
 from dataset.utils import get_hdf5_array
 
 
-class HypersimDataset(BaseDataset):
+class SintelDataset(BaseDataset):
     def __init__(self):
         super().__init__()
         meta_json = "/dataset/sharedir/research/Hypersim/valid_files.json"
@@ -51,42 +50,8 @@ class HypersimDataset(BaseDataset):
         return image, depth
 
 
-def convert_image_to_depth_path(image_path):
-    # 替换路径中的特定部分
-    new_path = image_path.replace(
-        "/image/training/final/",
-        "/depth/training/depth/"
-    )
-    # 修改文件扩展名为 .dpt
-    new_path = os.path.splitext(new_path)[0] + ".dpt"
-    return new_path
-
-
-def get_meta():
-    meta_json = '/dataset/sharedir/research/MPI-Sintel/meta_data.json'
-    image_root = "/dataset/sharedir/research/MPI-Sintel/image/training/final/"
-    idx = 0
-    for root, dir, files in os.walk(image_root):
-        for file in files:
-            image_path = os.path.join(root, file)
-            depth_path = convert_image_to_depth_path(image_path)
-            if (not os.path.exists(image_path)) or (not os.path.exists(depth_path)):
-                print(f"File not found: {image_path}")
-                continue
-            entry = {
-                "id": idx + 1,
-                "img_path": image_path,
-                "depth_path": depth_path
-            }
-
-            idx += 1
-            with open(meta_json, 'a') as f:
-                json.dump(entry, f)
-                f.write('\n')
-
-
 if __name__ == "__main__":
-    dataset = HypersimDataset()
+    dataset = SintelDataset()
     print(f"Dataset length: {len(dataset)}")
     for id, (image, depth) in enumerate(dataset):
         print(f"Id: {id}, Image shape: {image.shape}, Depth shape: {depth.shape}")
