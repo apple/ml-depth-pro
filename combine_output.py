f5c06a764761f704e2c7fb708038d7a0da26ceb2
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-13 11:24:22 +0800

    ?

---
+++ combine_output.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+import cv2
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import torch
+import src.depth_pro.depth_pro as depth_pro
+from dataset.HypersimDataset import HypersimDataset
+from dataset.SintelDataset import SintelDataset
+from dataset.utils import get_hdf5_array
+
+
+def get_dataset(dataset_name):
+    if dataset_name == "Hypersim":
+        return HypersimDataset()
+    elif dataset_name == "Sintel":
+        return SintelDataset()
+
+
+if __name__ == "__main__":
+    dataset_name = "Sintel"
+    save_root = r'/dataset/vfayezzhang/test/depth-pro/infer/vis/'
+    os.makedirs(save_root, exist_ok=True)
+    dataset = get_dataset(dataset_name)
+    model_names = ['marigold', 'dav2', 'depth-pro']
+
+    for id, (image, depth_gt) in enumerate(dataset):
+        image_numpy = image.cpu().numpy().transpose(1, 2, 0)
+        depth_gt_np = depth_gt.cpu().numpy()
+
+        # 图片列表
+        images = [image_numpy, depth_gt_np]
+        titles = ["Origin Image", "Ground Truth"]
+        for _model_name in model_names:
+            predict_depth_path = os.path.join(save_root, _model_name, dataset_name, f"{id}.png")
+            predict_depth = cv2.imread(predict_depth_path, cv2.IMREAD_GRAYSCALE)
+            images.append(predict_depth)
+            titles.append(_model_name)
+
+        # 创建一个水平拼接的图
+        fig, axes = plt.subplots(1, len(images), figsize=(30, 10))
+
+        for i, ax in enumerate(axes):
+            ax.imshow(images[i])
+            ax.set_title(titles[i])
+            ax.axis("off")
+
+        # 调整布局
+        plt.tight_layout()
+        plt.savefig(os.path.join(save_root, 'combine', f"{id}.png"))
---
+++ dataset/NYUDataset.py
@@ -0,0 +1,105 @@
+import json
+import os
+import pickle
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torchvision import transforms
+from dataset.BaseDataset import BaseDataset
+from dataset.utils import get_hdf5_array
+import cv2
+
+
+class SintelDataset(BaseDataset):
+    def __init__(self):
+        super().__init__()
+        meta_json = "/dataset/sharedir/research/MPI-Sintel/meta_data.json"
+        self.meta_json = meta_json
+        self.image_paths = []
+        self.depth_paths = []
+        self.depth_threshold = 200.
+        with open(meta_json, "r", encoding="utf-8") as infile:
+            for line in infile:
+                entry = json.loads(line)
+                self.image_paths.append(entry["img_path"])
+                self.depth_paths.append(entry["depth_path"])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def preproess(self, image_path, depth_path):
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image = image / 255.0
+
+        depth_info = open(depth_path, 'rb')
+        check = np.fromfile(depth_info, dtype=np.float32, count=1)[0]
+        width = np.fromfile(depth_info, dtype=np.int32, count=1)[0]
+        height = np.fromfile(depth_info, dtype=np.int32, count=1)[0]
+        size = width * height
+        depth = np.fromfile(depth_info, dtype=np.float32, count=-1).reshape((height, width))
+        # depth = np.clip(depth, 0.0, self.depth_threshold)
+
+        return image, depth
+
+    def __getitem__(self, idx):
+        '''
+        idx: list,int
+        Return:
+            image: torch.Tensor
+            depth: torch.Tensor
+        '''
+        if isinstance(idx, list):
+            return [self.__getitem__(i) for i in idx]
+        image_np, depth_np = self.preproess(self.image_paths[idx], self.depth_paths[idx])
+        to_tensor = transforms.ToTensor()
+        image = to_tensor(image_np)
+        depth = to_tensor(depth_np)
+        return image, depth
+
+
+def convert_image_to_depth_path(image_path):
+    # 替换路径中的特定部分
+    new_path = image_path.replace(
+        "/image/training/final/",
+        "/depth/training/depth/"
+    )
+    new_path = os.path.splitext(new_path)[0] + ".dpt"
+    return new_path
+
+
+def get_meta(meta_json):
+    image_root = "/dataset/sharedir/research/MPI-Sintel/image/training/final/"
+    idx = 0
+    for root, dir, files in os.walk(image_root):
+        for file in files:
+            image_path = os.path.join(root, file)
+            depth_path = convert_image_to_depth_path(image_path)
+            if (not os.path.exists(image_path)) or (not os.path.exists(depth_path)):
+                print(f"File not found: {image_path}")
+                continue
+            entry = {
+                "id": idx + 1,
+                "img_path": image_path,
+                "depth_path": depth_path
+            }
+            idx += 1
+            print("Saving to meta data:", entry)
+            with open(meta_json, 'a') as f:
+                json.dump(entry, f)
+                f.write('\n')
+
+
+if __name__ == "__main__":
+    meta_json = '/dataset/sharedir/research/MPI-Sintel/meta_data.json'
+
+    if not os.path.exists(meta_json):
+        get_meta(meta_json=meta_json)
+
+    dataset = SintelDataset()
+    print(f"Dataset length: {len(dataset)}")
+
+    for id, (image, depth) in enumerate(dataset):
+        print(f"Id: {id}, Image shape: {image.shape}, Depth shape: {depth.shape}")
