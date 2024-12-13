1e55135257f51ab258476ab684be0bf860cd9a91
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-13 15:51:40 +0800

    123

--- combine_output.py
+++ combine_output.py
@@ -1,28 +1,9 @@
-import json
-import os
-
-import cv2
-import numpy as np
-from PIL import Image
-from matplotlib import pyplot as plt
-import torch
-import src.depth_pro.depth_pro as depth_pro
-from dataset.HypersimDataset import HypersimDataset
-from dataset.NYUDataset import NYUDataset
-from dataset.SintelDataset import SintelDataset
-from dataset.utils import get_hdf5_array
-
-
-def get_dataset(dataset_name):
-    if dataset_name == "Hypersim":
-        return HypersimDataset()
-    elif dataset_name == "Sintel":
-        return SintelDataset()
-    elif dataset_name == "NYUv2":
-        return NYUDataset()
-
-
 if __name__ == "__main__":
+    import os
+    import cv2
+    import numpy as np
+    import matplotlib.pyplot as plt
+
     # dataset_name = "Sintel"
     dataset_names = ['NYUv2']
     res_root = r'/dataset/vfayezzhang/test/depth-pro/infer/vis/'
@@ -37,10 +18,11 @@ if __name__ == "__main__":
             image_numpy = image.cpu().numpy().transpose(1, 2, 0)
             depth_gt_np = depth_gt.squeeze().cpu().numpy()
 
-            # 图片列表
+            # Image list and titles
             images = [image_numpy, depth_gt_np]
-            titles = ["Origin Image", "Ground Truth"]
+            titles = ["Original Image", "Ground Truth"]
             flag = False
+
             for _model_name in model_names:
                 predict_depth_path = os.path.join(res_root, _model_name, dataset_name, f"{id}.png")
                 predict_depth = cv2.imread(predict_depth_path, cv2.IMREAD_GRAYSCALE)
@@ -54,15 +36,16 @@ if __name__ == "__main__":
             if flag:
                 continue
 
-            # 创建一个水平拼接的图
-            fig, axes = plt.subplots(1, len(images), figsize=(30, 10))
+            # Create a grid for two rows and three columns
+            fig, axes = plt.subplots(2, 3, figsize=(15, 10))
 
-            for i, ax in enumerate(axes):
-                ax.imshow(images[i])
-                ax.set_title(titles[i])
-                ax.axis("off")
+            for i, ax in enumerate(axes.flatten()):
+                if i < len(images):
+                    ax.imshow(images[i], cmap='gray' if i > 0 else None)
+                    ax.set_title(titles[i])
+                ax.axis("off")  # Turn off axes for all subplots
 
-            # 调整布局
+            # Adjust layout and save the figure
             plt.tight_layout()
-
             plt.savefig(os.path.join(save_root, f"{id}.png"))
+            plt.close(fig)
