36b4f736e95a902ef404714123245cea3e1660ce
SarahAnberlin <sarahanberlin@outlook.com>
2025-01-02 17:50:22 +0800

    123

--- dataset/HypersimDataset.py
+++ dataset/HypersimDataset.py
@@ -102,8 +102,8 @@ if __name__ == "__main__":
             image = get_hdf5_array(image_path)
             depth = get_hdf5_array(depth_path)
             # count the percentage of values larger than 1
-            img_invalid_percentage = ((image > 1).sum() / (image > - 0x3ff).sum()) + (image < 0).sum() / (image <
-                                                                                                          0x3ff).sum()
+            img_invalid_percentage = (((image > 1).sum() / (image > - 0x3ff).sum()) +
+                                      (image < 0).sum() / (image < 0x3ff).sum())
             print(f"Image {id} invalid percentage: {img_invalid_percentage}")
             if img_invalid_percentage > threshold:
                 with open(f'Invalid Image {threshold}.json', 'a') as f:
