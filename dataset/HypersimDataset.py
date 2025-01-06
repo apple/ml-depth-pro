106fd747cbde70f32f529fb45e79c58f1289ea5d
SarahAnberlin <sarahanberlin@outlook.com>
2025-01-06 16:12:00 +0800

    123

--- dataset/HypersimDataset.py
+++ dataset/HypersimDataset.py
@@ -1,4 +1,6 @@
 import json
+
+import torchvision
 from joblib import Parallel, delayed
 import json
 import numpy as np
@@ -34,25 +36,43 @@ class HypersimDataset(BaseDataset):
         self.meta_json = meta_json
         self.image_paths = []
         self.depth_paths = []
+        self.illum_paths = []
+        self.reflect_paths = []
         self.depth_threshold = 200.
         with open(meta_json, "r", encoding="utf-8") as infile:
             for line in infile:
                 entry = json.loads(line)
-                self.image_paths.append(entry["img_path"])
-                self.depth_paths.append(entry["depth_path"])
+                img_path = entry["img_path"]
+                illum_path = img_path.replace('.color.', '.diffuse_illumination.')
+                reflect_path = img_path.replace('.color.', '.diffuse_reflectance.')
+                depth_path = entry["depth_path"]
+                self.image_paths.append(img_path)
+                self.illum_paths.append(illum_path)
+                self.reflect_paths.append(reflect_path)
+                self.depth_paths.append(depth_path)
 
     def __len__(self):
         return len(self.image_paths)
 
-    def preproess(self, image_path, depth_path):
+    def preproess(self, image_path, depth_path, illum_path=None, reflect_path=None):
         image = get_hdf5_array(image_path)
         depth = get_hdf5_array(depth_path)
         # image = (image - image.min()) / (image.max() - image.min() + 1e-6)
         print('-------------raw image', image.shape, image.max(), image.min())
         image = np.clip(image, 0, 1.0)
         depth = np.clip(depth, 0, self.depth_threshold)
+        illum, reflect = None, None
+        if illum_path is not None:
+            illum = get_hdf5_array(illum_path)
+            print(f"Range of illum: {illum.min(), illum.max()}")
+            illum = np.clip(illum, 0, 1.0)
 
-        return image, depth
+        if reflect_path is not None:
+            reflect = get_hdf5_array(reflect_path)
+            print(f"Range of reflect: {reflect.min(), reflect.max()}")
+            reflect = np.clip(reflect, 0, 1.0)
+
+        return image, depth, illum, reflect
 
     def __getitem__(self, idx):
         '''
@@ -63,12 +83,17 @@ class HypersimDataset(BaseDataset):
         '''
         # if isinstance(idx, list):
         #     return [self.__getitem__(i) for i in idx]
-        image_np, depth_np = self.preproess(self.image_paths[idx], self.depth_paths[idx])
+        image_np, depth_np, illum, reflect = self.preproess(self.image_paths[idx], self.depth_paths[idx],
+                                                            self.illum_paths[idx], self.reflect_paths[idx])
         to_tensor = transforms.ToTensor()
         image = to_tensor(image_np)
         depth = to_tensor(depth_np)
+        if illum is not None:
+            illum = to_tensor(illum)
+        if reflect is not None:
+            reflect = to_tensor(reflect)
         # depth = depth_normalize(depth)
-        return image, depth
+        return image, depth, illum, reflect
 
 
 def clean():
@@ -126,24 +151,31 @@ def clean():
 
 
 if __name__ == "__main__":
-    clean()
+    # clean()
     # input()
     # import torchvision
     #
-    # dataset = HypersimDataset()  #
-    # print(f"Dataset length: {len(dataset)}")
-    # idx = 0
-    # for id in range(len(dataset)):
-    #     image, depth = dataset[idx]
-    #     print(f"Id: {idx}, Image shape: {image.shape}, Depth shape: {depth.shape}")
-    #     print(image.max(), image.min(), image.mean())
-    #     print(depth.max(), depth.min(), depth.mean())
-    #     save_root = 'vis/hypersim'
-    #     if not os.path.exists(save_root):
-    #         os.makedirs(save_root)
-    #     image_save_path = os.path.join(save_root, f'{idx}_image.png')
-    #     depth_save_path = os.path.join(save_root, f'{idx}_depth.png')
-    #     torchvision.utils.save_image(image.unsqueeze(0), image_save_path)
-    #     max_v, min_v = depth.max(), depth.min()
-    #     torchvision.utils.save_image(((depth - min_v) / (max_v - min_v)).unsqueeze(0), depth_save_path)
-    #     idx = int(input('input idx:'))
+    dataset = HypersimDataset()  #
+    print(f"Dataset length: {len(dataset)}")
+    idx = 0
+    for id in range(len(dataset)):
+        image, depth, illum, reflect = dataset[id]
+
+        print(f"Id: {idx}, Image shape: {image.shape}, Depth shape: {depth.shape}")
+        print(image.max(), image.min(), image.mean())
+        print(depth.max(), depth.min(), depth.mean())
+        save_root = 'vis/hypersim'
+        if not os.path.exists(save_root):
+            os.makedirs(save_root)
+        image_save_path = os.path.join(save_root, f'{idx}_image.png')
+        depth_save_path = os.path.join(save_root, f'{idx}_depth.png')
+        illum_save_path = os.path.join(save_root, f'{idx}_illum.png')
+        reflect_save_path = os.path.join(save_root, f'{idx}_reflect.png')
+
+        torchvision.utils.save_image(image.unsqueeze(0), image_save_path)
+        torchvision.utils.save_image(illum.unsqueeze(0), illum_save_path)
+        torchvision.utils.save_image(reflect.unsqueeze(0), reflect_save_path)
+
+        max_v, min_v = depth.max(), depth.min()
+        torchvision.utils.save_image(((depth - min_v) / (max_v - min_v)).unsqueeze(0), depth_save_path)
+        idx = int(input('input idx:'))
